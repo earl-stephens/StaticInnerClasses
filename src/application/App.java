f7c0f40dc889a1eac65635cc3137b3207299e998
earl-stephens <stephensearl@hotmail.com>
2022-02-15 23:03:13 -0700

    Demonstrate static inner class usage

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,14 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person person = new Person();
+		person.print();
 
+		/*Have to use this syntax since Head is an inner class
+		 * inside Person
+		 */
+		Person.Head head = new Person.Head();
+		head.print();
 	}
 
 }
